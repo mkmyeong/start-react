6929022f9dbeef171663c8ab41e65d6938efd81d
mkmyeong <mkmyeong@gmail.com>
2021-10-23 17:50:45 +0900

    Add  dummyData

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  padding: 20px 50px;
+  margin: 0 auto;
+`;
+
+const QuestionCard = styled.p`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const IsShowButton = styled.button`
+  background-color: blue;
+  color: white;
+  border-radius: 8px;
+  border: none;
+  padding: 2px 10px;
+`;
+
+const dummyData = [
+  {
+    id: 1,
+    question: '성왕은 도읍을 *로 옮기고, 국호를 *로 바꾸었다.',
+    answer: ['사비', '남부여'],
+    isShow: false,
+  },
+  {
+    id: 2,
+    question: '신라의 왕호는 * → * → * → * → 왕으로 변하였다.',
+    answer: ['거서간', '차차웅', '이사금', '마립간'],
+    isShow: false,
+  },
+  {
+    id: 3,
+    question: '신라의 *은 국방부에 해당하는 *를 설치하였다.',
+    answer: ['법흥왕', '병부'],
+    isShow: false,
+  },
+];
 
 function App() {
+  const [questionList, setQuestionList] = useState(dummyData);
+
+  const onMouseEnterHandler = (id) => {
+    setQuestionList(
+      questionList.map((q) => (q.id === id ? { ...q, isShow: true } : q)),
+    );
+  };
+
+  const onMouseLeaveHandler = (id) => {
+    setQuestionList(
+      questionList.map((q) => (q.id === id ? { ...q, isShow: false } : q)),
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      {questionList.map((q) => (
+        <QuestionCard key={q.id}>
+          {q.isShow
+            ? q.answer
+            : q.question.replaceAll('*', '(\u00A0\u00A0\u00A0)')}
+          <IsShowButton
+            onMouseEnter={() => onMouseEnterHandler(q.id)}
+            onMouseLeave={() => onMouseLeaveHandler(q.id)}
+          >
+            보기
+          </IsShowButton>
+        </QuestionCard>
+      ))}
+    </Container>
   );
 }
 
